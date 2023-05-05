74f6b6deca31c09af6ac4cda4deb76660bc66b29
GiulianaCDA <giulianaarecippo@hotmail.com>
2023-05-05 00:22:40 -0300

    adiciona página de seleção de features

---
+++ news-portal/src/app/pages/features-form/features-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-features-form',
+  templateUrl: './features-form.component.html',
+  styleUrls: ['./features-form.component.scss']
+})
+export class FeaturesFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
