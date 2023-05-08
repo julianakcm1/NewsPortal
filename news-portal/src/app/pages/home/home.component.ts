94b889a3d835c7fffffc255bce35eb276c6f1509
GiulianaCDA <giulianaarecippo@hotmail.com>
2023-05-08 01:44:23 -0300

    integra home

--- news-portal/src/app/pages/home/home.component.ts
+++ news-portal/src/app/pages/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from 'src/app/api.service';
 
+import { News } from 'src/app/interfaces/news-portal.interface';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -8,6 +9,9 @@ import { ApiService } from 'src/app/api.service';
 })
 export class HomeComponent implements OnInit {
   news!: any
+  firstRecentNews!: News
+  secondRecentNews!: News
+  thirdRecentNews!: News
 
   constructor(
     private apiService: ApiService
@@ -16,6 +20,13 @@ export class HomeComponent implements OnInit {
   ngOnInit(): void {
     this.apiService.getNews().subscribe(response => {
       this.news = response
+      const recentNews = this.news.length
+      this.firstRecentNews = this.news[recentNews-1]
+      this.secondRecentNews = this.news[recentNews-2]
+      this.thirdRecentNews = this.news[recentNews-3]
+      this.news.pop()
+      this.news.pop()
+      this.news.pop()
     });
   }
 
